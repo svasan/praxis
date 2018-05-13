a3c3bc15b31d25f9c15775462e5ca55f20c7e62c
Sundar Vasan <vr.sundar@gmail.com>
2018-05-13 11:07:01 -0700

    DirectedCycle detects cycles in directed graphs.
        $ ruby directed_cycle.rb tinyDG.txt
        Graph has 5 cycle(s): [[3,2,3], [3,5,4,2,3], [2,0,5,4,2], [8,6,8], [12,9,10,12]]
    
    (Implemented about a week ago; committing it now.)

---
+++ graph/directed_cycle.rb
@@ -0,0 +1,58 @@
+require_relative '../ds/stack'
+
+class DirectedCycle
+  def initialize(graph)
+    @graph = graph
+    @marked = Array.new(graph.size)
+    @on_stack = Array.new(graph.size)
+    @edge_to = Array.new(graph.size)
+    @cycle = []
+    @graph.each_vertex do |v|
+      if not @marked[v]
+        @edge_to[v] = v
+        dfs(v)
+      end
+    end
+  end
+
+  def dfs(v)
+    @marked[v] = true
+    @on_stack << v
+    @graph.each_edge(v) do |w|
+      if not @marked[w]
+        @edge_to[w] = v
+        dfs(w)
+      elsif @on_stack.include?(w)
+        cycle = Stack.new
+        n = v
+        until n == w
+          cycle << n
+          n = @edge_to[n]
+        end
+        cycle << w
+        cycle << v
+        @cycle << cycle
+      end
+    end
+    @on_stack.pop
+  end
+
+  def cycle?
+    not @cycle.empty?
+  end
+
+  def cycle
+    @cycle
+  end
+end
+
+if __FILE__ == $0
+  require_relative 'digraph'
+  g = Digraph.fromFile(ARGV[0])
+  dc = DirectedCycle.new(g)
+  if dc.cycle?
+    puts "Graph has #{dc.cycle.size} cycle(s): #{dc.cycle}"
+  else
+    puts "Graph does not have a cycle"
+  end
+end
