d6904b72da499b71c140f13c7ace110dba299de6
Sundar Vasan <vr.sundar@gmail.com>
2018-05-13 19:30:14 -0700

    Represent an edge weighted undirected graph using adjacency lists.
    EdgeWeightedGraph uses an Edge object to represent edges which also
    stores the weight of that edge. Other than that, is similar to
    UndirectedGraph.
    
    Use ARGF to work with a file argument or stdin.
    
        $ ruby edge_weighted_graph.rb tinyEWG.txt
        # Edited to format
        {0=>[[0-7 @ 0.16], [0-4 @ 0.38], [0-2 @ 0.26], [6-0 @ 0.58]],
         1=>[[1-5 @ 0.32], [1-7 @ 0.19], [1-2 @ 0.36], [1-3 @ 0.29]],
         2=>[[2-3 @ 0.17], [0-2 @ 0.26], [1-2 @ 0.36], [2-7 @ 0.34], [6-2 @ 0.4]],
         3=>[[2-3 @ 0.17], [1-3 @ 0.29], [3-6 @ 0.52]],
         4=>[[4-5 @ 0.35], [4-7 @ 0.37], [0-4 @ 0.38], [6-4 @ 0.93]],
         5=>[[4-5 @ 0.35], [5-7 @ 0.28], [1-5 @ 0.32]],
         6=>[[6-2 @ 0.4], [3-6 @ 0.52], [6-0 @ 0.58], [6-4 @ 0.93]],
         7=>[[4-7 @ 0.37], [5-7 @ 0.28], [0-7 @ 0.16], [1-7 @ 0.19], [2-7 @ 0.34]]}

---
+++ graph/edge_weighted_graph.rb
@@ -0,0 +1,105 @@
+require 'set'
+
+class EdgeWeightedGraph
+
+  class Edge
+    include Comparable
+
+    def initialize(v, w, wt)
+      @v = v
+      @w = w
+      @weight = wt
+    end
+
+    def weight; @weight; end
+    def either; @v; end
+
+    def other(v)
+      return @w if v == @v
+      return @v if v == @w
+
+      raise RuntimeError, "Edge #{v} doesn't match either end - #{@v} or #{@w}"
+    end
+
+    def <=>(other)
+      @weight <=> other.weight
+    end
+
+    def to_s
+      "[#{@v}-#{@w} @ #{@weight}]"
+    end
+    alias_method :inspect, :to_s
+
+  end
+
+  attr_reader :nedges
+
+  def initialize(n)
+    @vertices = Array.new(n) {Set.new()}
+    @nedges = 0
+  end
+
+  def nvertices()
+    @vertices.size()
+  end
+
+  alias_method :size, :nvertices
+
+  def edges(v)
+    @vertices[v]
+  end
+
+  def add_edge(e)
+    v = e.either(); w = e.other(v);
+
+    @vertices[v] << e
+    @vertices[w] << e
+    @nedges += 1
+  end
+
+  def each_edge(v)
+    @vertices[v].each do |edge|
+      yield edge
+    end
+  end
+
+  def each_vertex()
+    0.upto(@vertices.size() - 1) do |i|
+      yield i
+    end
+  end
+
+  def each_vertex_with_edges()
+    @vertices.each_with_index do |edges, i|
+      yield i, edges
+    end
+  end
+
+  def to_s
+    s = "{"
+    each_vertex_with_edges do |v, edges|
+      s += "#{v}=>#{edges.to_a}, " if not edges.empty?
+    end
+    s.strip!.chomp!(',')
+    s += "}"
+    s
+  end
+
+  def self.fromFile(io)
+    nv = io.readline.to_i
+    ne = io.readline.to_i
+    g = EdgeWeightedGraph.new(nv)
+    1.upto(ne) do
+      from, to, weight = io.readline.split
+      g.add_edge(Edge.new(from.to_i, to.to_i, weight.to_f))
+    end
+    g
+  end
+
+end
+
+
+if __FILE__ == $0
+  ewg = EdgeWeightedGraph.fromFile(ARGF)
+  puts ewg.to_s
+end
