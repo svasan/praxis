483ab3e5eb7f5224d14d342b695aa7cd9aacc9de
Sundar Vasan <vr.sundar@gmail.com>
2019-06-18 17:10:16 -0700

    Add webpack and a simple project
    The project shows how export and import work with webpack and includes
    a sample html.

---
+++ webpack_from_nothing/README.md
@@ -0,0 +1,1 @@
+This directory contains examples from https://what-problem-does-it-solve.com/webpack/index.html
---
+++ webpack_from_nothing/js/index.js
@@ -0,0 +1,6 @@
+console.log("Hello from index.js");
+import address from "./address";
+import billing from "./billing";
+
+address.announce();
+billing.announce();
