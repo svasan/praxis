a51404c17f88f484fdda8a7ce550b61886ad40c2
Sundar Vasan <vr.sundar@gmail.com>
2019-06-18 20:05:42 -0700

    Generate script tag based on the build type
    This uses html-webpack-plugin and lets us use the correct bundle when
    the hash changes.

--- webpack_from_nothing/config/webpack.common.config.js
+++ webpack_from_nothing/config/webpack.common.config.js
@@ -1,4 +1,11 @@
+const HtmlPlugin = require("html-webpack-plugin");
+
 module.exports = {
+  plugins: [
+    new HtmlPlugin({
+      template: "./html/index.html"
+    })
+  ],
   entry: "./js/index.js",
   mode: "none"
 };
